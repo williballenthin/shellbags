d0ac052372d57b37e3703b7972ddaa591ec09f6e
Willi Ballenthin <william.ballenthin@mandiant.com>
2013-12-19 23:57:32 -0500

    add more thorough output to test for issue #7.

--- tests/Issue7/issue7.py
+++ tests/Issue7/issue7.py
@@ -15,14 +15,53 @@ from ShellItems import SHITEMLIST
 sys.path.pop()
 
 
+def hex_dump(data):
+    """
+    see http://code.activestate.com/recipes/142812/
+    """
+    byte_format = {}
+    for c in xrange(256):
+        if c > 126:
+            byte_format[c] = '.'
+        elif len(repr(chr(c))) == 3 and chr(c):
+            byte_format[c] = chr(c)
+        else:
+            byte_format[c] = '.'
+
+    def format_bytes(s):
+        return "".join([byte_format[ord(c)] for c in s])
+
+    def dump(src, length=16):
+        N = 0
+        result = ''
+        while src:
+            s, src = src[:length], src[length:]
+            hexa = ' '.join(["%02X" % ord(x) for x in s])
+            s = format_bytes(s)
+            result += "%04X   %-*s   %s\n" % (N, length * 3, hexa, s)
+            N += length
+        return result
+    return dump(data)
+
+
 def test(filename):
     r = Registry.Registry(filename)
     k = r.open("Local Settings\\Software\\Microsoft\\Windows\\Shell\\BagMRU\\1\\0\\0")
     v = k.value("0")
 
+    print hex_dump(v.value())
+
     l = SHITEMLIST(v.value(), 0, False)
-    for item in l.items():
-        print item.name()
+    for index, item in enumerate(l.items()):
+        print "item:", index
+        print "type:", item.__class__.__name__
+        print "name:", item.name()
+
+        # its a SHITEM_FILEENTRY
+        print "short name:", item.short_name()
+        print "off long name:", item._off_long_name
+        print "off long name size:", item._off_long_name_size
+        print "long name size:", hex(item.long_name_size())
 
 
 def main():
